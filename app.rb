4000615709e17cace911aa6f4ec44dc130caad7f
kylewstewart <kyle.w.stewart@mac.com>
2017-04-26 17:32:43 -0400

    Yeah

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    configure do
+      set :views, 'views/pirates'
+    end
+
+    get '/' do
+      erb :index
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ship1 = Ship.new(params[:pirate][:ships][0])
+      @ship2 = Ship.new(params[:pirate][:ships][1])
+
+      erb :show
+    end
 
   end
+
 end
